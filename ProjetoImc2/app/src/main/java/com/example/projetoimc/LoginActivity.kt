4d454b1d9ac6807217f0a23e2155bd9af84a2857
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-10-04 11:25:04 -0300

    Ligação login - cadastro

--- ProjetoImc2/app/src/main/java/com/example/projetoimc/LoginActivity.kt
+++ ProjetoImc2/app/src/main/java/com/example/projetoimc/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.example.projetoimc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +11,14 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
 
         supportActionBar!!.hide()
+
+        val tvNovaConta = findViewById<TextView>(R.id.tv_nova_conta)
+
+        tvNovaConta.setOnClickListener {
+            val abrirCriarPerfil = Intent (this, CadastroUsuarioActivity::class.java)
+
+            startActivity(abrirCriarPerfil)
+        }
 
     }
 }
